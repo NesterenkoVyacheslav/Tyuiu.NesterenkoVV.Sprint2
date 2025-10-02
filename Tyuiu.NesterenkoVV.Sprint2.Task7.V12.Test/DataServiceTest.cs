dd50037eb402d48cedbf2f89a132bb5bbe1cf885
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-10-02 23:54:41 +0500

    Task7

---
+++ Tyuiu.NesterenkoVV.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NesterenkoVV.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds= new DataService();
+            double x = 1;
+            double y = 1;
+            bool wait = true;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NesterenkoVV.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint2.Task7.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу на C#, которая запрашивает исходные данные и          *");
+            Console.WriteLine("*вычисляет, находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*дату следующего дня                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите x");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            y = Convert.ToDouble(Console.ReadLine());
+            var res = ds.CheckDotInShadedArea(x, y); 
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
